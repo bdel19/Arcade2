40c89d67678c8b05cb383f34b944ea466fb1255f
bdel19 <bmd21991@yahoo.com>
2023-03-19 20:47:20 -0500

    finish project

--- tictactoe.js
+++ tictactoe.js
@@ -9,15 +9,35 @@ const state = {
     player2Name: '',
     player: "X",
     playerTurn: ""
-}
+};
+
+
+let clickAmount = 0;
+
+// const winConditions = [
+//     [0,1,2],
+//     [3,4,5],
+//     [6,7,8],
+//     [0,3,6],
+//     [1,4,7],
+//     [2,5,8],
+//     [0,4,8],
+//     [2,4,6]
+// ];
+
+// let winOptions =["","","","","","","","",""]
 
 const board = document.querySelector('#board');
 board.style.display = "none";
 
+
+
+const scoreboard = document.querySelector("#scoreboard");
+
 const player1 = document.querySelector('#player1');
 const player2 = document.querySelector('#player2');
 
-const cell = document.querySelector('.cell');
+const cell = document.querySelectorAll('.cell');
 
 const subBtn = document.getElementById('submit');
 
@@ -28,6 +48,20 @@ const playerAssign = document.querySelector('#playerAssign');
 
 const victor = document.querySelector('#victor');
 victor.style.display = "none";
+
+const restartBtn = document.querySelector("#restart");
+restartBtn.style.display="none";
+
+restartBtn.addEventListener('click', function(ev){   
+    for( i = 0; i = board.length-1; i ++){
+        console.log(cell.innerText)}
+        board.style.display = "flex";
+        currentPlayer.style.display="flex";
+        victor.style.display="none";
+        restartBtn.style.display="none";
+        restartGame();
+}
+)
 
 const cell1 = document.querySelector('#cell1');
 const cell2 = document.querySelector('#cell2');
@@ -55,11 +89,13 @@ subBtn.addEventListener('click', function(ev){
 })
 
 
-
 //checks if cell has been played, inserts marker into cell, changes player
 board.addEventListener('click', function (ev){
+    console.log(ev.target)
+    clickAmount += 1;
     if(ev.target.innerText === ""){
         ev.target.innerText = state.player;
+        checkWinner();
         if(state.player === "X"){
             state.player = "O"
             playerTurn = state.player2Name
@@ -68,20 +104,75 @@ board.addEventListener('click', function (ev){
             state.player = "X";
             playerTurn = state.player1Name;
         }
+        
     }
     currentPlayer.innerText = `It is ${playerTurn}'s turn!`
-    topLeftToBottomRight();
-    console.log(board[i][i])
-    console.log(state.player);
 })
 
-// cell.addEventListener('mouseenter', function(ev){
-//     ev.target.innerText = state.player;
-//     console.log("Hover event")
-// })
+function checkWinner(){
+    if(
+        (cell1.innerText === "X" && cell2.innerText === "X" && cell3.innerText === "X")
+        ||
+        (cell4.innerText === "X" && cell5.innerText === "X" && cell6.innerText === "X")
+        ||
+        (cell7.innerText === "X" && cell8.innerText === "X" && cell9.innerText === "X")
+        ||
+        (cell1.innerText === "X" && cell4.innerText === "X" && cell7.innerText === "X")
+        ||
+        (cell2.innerText === "X" && cell5.innerText === "X" && cell8.innerText === "X")
+        ||
+        (cell3.innerText === "X" && cell6.innerText === "X" && cell9.innerText === "X")
+        ||
+        (cell1.innerText === "X" && cell5.innerText === "X" && cell9.innerText === "X")
+        ||
+        (cell3.innerText === "X" && cell5.innerText === "X" && cell7.innerText === "X")
+        ||
+        (cell1.innerText === "O" && cell2.innerText === "O" && cell3.innerText === "O")
+        ||
+        (cell4.innerText === "O" && cell5.innerText === "O" && cell6.innerText === "O")
+        ||
+        (cell7.innerText === "O" && cell8.innerText === "O" && cell9.innerText === "O")
+        ||
+        (cell1.innerText === "O" && cell4.innerText === "O" && cell7.innerText === "O")
+        ||
+        (cell2.innerText === "O" && cell5.innerText === "O" && cell8.innerText === "O")
+        ||
+        (cell3.innerText === "O" && cell6.innerText === "O" && cell9.innerText === "O")
+        ||
+        (cell1.innerText === "O" && cell5.innerText === "O" && cell9.innerText === "O")
+        ||
+        (cell3.innerText === "O" && cell5.innerText === "O" && cell7.innerText === "O")){
+            board.style.display = "none";
+            currentPlayer.style.display="none";
+            victor.style.display="flex";
+            victor.innerText = `${playerTurn} has won!`
+            restartBtn.style.display="flex";
 
-// function topLeftToBottomRight() {
-    //     for (let i = 0; i < board.length; i++) {
-    //       console.log(board[i][i])
-    //     }
-    //   }
+
+    }else if(clickAmount === 9){
+        board.style.display = "none";
+            currentPlayer.style.display="none";
+            victor.style.display="flex";
+            victor.innerText = "Draw!"
+            restartBtn.style.display="flex";
+
+    }
+    
+
+}
+
+function restartGame(){
+    cell1.innerText="";
+    cell2.innerText="";
+    cell3.innerText="";
+    cell4.innerText="";
+    cell5.innerText="";
+    cell6.innerText="";
+    cell7.innerText="";
+    cell8.innerText="";
+    cell9.innerText="";
+    playerTurn = state.player1Name
+    currentPlayer.innerText = `It is ${playerTurn}'s turn!`;
+    clickAmount=0 
+}
+
